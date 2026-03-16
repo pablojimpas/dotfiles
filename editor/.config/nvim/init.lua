d0462233e5983f156759501e6f96362a9ed7e088
Pablo Jiménez Pascual <pablo@jimpas.me>
2026-03-16 18:19:43 +0100

    Revert "Back to acme theme in neovim"
    This reverts commit 00b1a009bae84d8b830ced35ef41b00453743934.

--- editor/.config/nvim/init.lua
+++ editor/.config/nvim/init.lua
@@ -71,9 +71,41 @@ require('lazy').setup({
   },
 
   {
-    'pablojimpas/acme-colors',
-    name = 'acme',
+    'catppuccin/nvim',
+    name = 'catppuccin',
     priority = 1000,
+  },
+  -- {
+  --   'pablojimpas/acme-colors',
+  --   name = 'acme',
+  --   priority = 1000,
+  -- },
+
+  {
+    -- Set lualine as statusline
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    -- See `:help lualine.txt`
+    opts = {
+      options = {
+        icons_enabled = true,
+        theme = 'catppuccin',
+
+        section_separators = { left = '', right = '' },
+        component_separators = { left = '', right = '' }
+        -- component_separators = '',
+        -- section_separators = '',
+      },
+    },
+  },
+
+  {
+    -- Add indentation guides even on blank lines
+    'lukas-reineke/indent-blankline.nvim',
+    -- Enable `lukas-reineke/indent-blankline.nvim`
+    -- See `:help ibl`
+    main = 'ibl',
+    opts = {},
   },
 
   -- "gc" to comment visual regions/lines
@@ -136,12 +168,12 @@ require('lazy').setup({
     },
   },
 
-  -- {
-  --   -- Highlight, edit, and navigate code
-  --   'nvim-treesitter/nvim-treesitter',
-  --   dependencies = {
-  --     'nvim-treesitter/nvim-treesitter-textobjects',
-  --   },
-  --   build = ':TSUpdate',
-  -- },
+  {
+    -- Highlight, edit, and navigate code
+    'nvim-treesitter/nvim-treesitter',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter-textobjects',
+    },
+    build = ':TSUpdate',
+  },
 }, {})
